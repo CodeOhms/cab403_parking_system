c227598e29fb5a241f2f805d7c88edef31dc11e6
Yorikaryl <87790076+Yorikaryl@users.noreply.github.com>
2021-10-08 13:24:42 +1000

    Update car_park_manager.c

--- src/car_park_manager.c
+++ src/car_park_manager.c
@@ -1,5 +1,18 @@
 
 int main(void)
 {
-    
+    // Car Status
+        // Monitor Status of LPR Sensor and Keep track of each char
+
+        // Ensure that there is room in car park
+
+        // Keep Track of car parking
+
+    // Boom Gates
+        // Tell Boom gate to open
+
+    // Displaying Information
+        // Signs Display
+
+        // Display current status of parking 
 }
