5a078bd918c89cbbe69a732b3dcd68fe08c16d98
Yorikaryl <87790076+Yorikaryl@users.noreply.github.com>
2021-10-08 13:32:33 +1000

    Update car_park_simulator.c

--- src/car_park_simulator.c
+++ src/car_park_simulator.c
@@ -1,5 +1,28 @@
 
 int main(void)
 {
-    // Test
+
+    // Simulating Car
+        // Generate Random License Plate
+
+        // Car Queue up for entrance triggering LPR
+
+        // Get information from digital sign
+
+        // Respond to Information received from sign
+
+        // Generate Random Time to park
+
+        // Leave after finish parking
+
+        // Remove Car from Simulation
+
+    // Simulate Boom Gate
+        // Get signal to open
+
+        // Close Gate after time
+
+    // Simulate Temperature
+        // Generate Temperature
+        
 }
